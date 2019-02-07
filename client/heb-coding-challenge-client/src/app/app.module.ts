7d258d9d607065643f452c787d22f7efa1e35228
Tony Andrade <tandrade@onlyonestone.com>
2019-02-06 21:23:35 -0600

    Adding ngrx and effects

--- client/heb-coding-challenge-client/src/app/app.module.ts
+++ client/heb-coding-challenge-client/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductComponent } from './product/product.component';
 import { FooterComponent } from './footer/footer.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
    declarations: [
@@ -14,7 +18,9 @@ import { FooterComponent } from './footer/footer.component';
       FooterComponent
    ],
    imports: [
-      BrowserModule
+      BrowserModule,
+      StoreModule.forRoot(reducers, { metaReducers }),
+      EffectsModule.forRoot([AppEffects])
    ],
    providers: [],
    bootstrap: [
