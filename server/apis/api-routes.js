e7521a38ea5e649df0ab97ead54d4aeb2e1b1677
Tony Andrade <tandrade@onlyonestone.com>
2019-02-07 22:44:00 -0600

    Backup connection string to mongodb atlas cloud

--- server/apis/api-routes.js
+++ server/apis/api-routes.js
@@ -1,17 +1,18 @@
-// Initialize express router
+// Import and intialize express router
 let router = require("express").Router();
+
 // Set default API response
 router.get("/", function(req, res) {
     res.json({
         status: "API Its Working",
-        message: "Welcome to HEB!"
+        message: "Welcome to HEB's coding challenge!"
     });
 });
 
 // Import product controller
 var productController = require("../controllers/productController");
 
-// Contact routes
+// API routes
 router.route("/products").get(productController.index);
 
 // Export API routes
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// got to give props to David Inyang-Etoh for his amazing mongo node express rest tutorial 
+// got to give props to David Inyang-Etoh for his amazing mongo node express rest tutorial
 // https://medium.com/@dinyangetoh/how-to-build-simple-restful-api-with-nodejs-expressjs-and-mongodb-99348012925d
 
 // Imports
@@ -10,19 +10,27 @@ const HOST_URL = "localhost:27017";
 const DB_NAME = "heb";
 const port = process.env.PORT || 8080;
 
+//incase my comp stops working and I need to borrow a laptop that doesn't have mongodb installed -- Keeping credentials in my head for Reasons ;)
+// const mongoCloudConnection =
+//     "mongodb://<userName>:<Password>@cluster0-shard-00-00-dojwr.mongodb.net:27017,cluster0-shard-00-01-dojwr.mongodb.net:27017,cluster0-shard-00-02-dojwr.mongodb.net:27017/heb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+
 // Initialize the app
 let app = express();
 let apiRoutes = require("./apis/api-routes");
 
-app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-);
-app.use(bodyParser.json());
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: true
+//     })
+// );
+// app.use(bodyParser.json());
+
+//connection string for just in case 
+//mongoose.connect(mongoCloudConnection)
 
 // Connect to Mongoose and set connection variable
 mongoose.connect(`mongodb://${HOST_URL}/${DB_NAME}`);
+
 var db = mongoose.connection;
 
 //default route
