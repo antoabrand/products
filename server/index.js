035ed42452bd03b730e3a188948802bb2538e6f5
Tony Andrade <aandrade0205@gmail.com>
2019-02-07 19:45:54 -0600

    Added fancy middleware mongoose and some better mvc pattern folder structure

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
-// FileName: index.js
-// Import express
+//imports 
 let express = require('express')
+let apiRoutes = require('./apis/api-routes');
+
 // Initialize the app
 let app = express();
+
+// Use Api routes in the App
+app.use('/api', apiRoutes);
+
 // Setup server port
 var port = process.env.PORT || 8080;
-// Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));
+
+//default route
+app.get('/', (req, res) => res.send('HEB Challenge API server!'));
+
 // Launch app to listen to specified port
 app.listen(port, function () {
      console.log("Running HEB-Challenge server on port " + port);
