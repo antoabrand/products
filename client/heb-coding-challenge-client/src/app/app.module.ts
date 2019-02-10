b0842aa0577acc892a9b338ce3df31f843d03908
Tony Andrade <aandrade0205@gmail.com>
2019-02-10 14:42:19 -0600

    So much closer

--- client/heb-coding-challenge-client/src/app/app.module.ts
+++ client/heb-coding-challenge-client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {MaterialModule} from './material.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,6 +14,7 @@ import { AppEffects } from './app.effects';
 import { ProductEffects } from './ngrx/product/product.effects';
 import { stateSetter } from './ngrx/meta-reducers/set-intial-state-reducer';
 import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent, FooterComponent],
@@ -21,7 +23,9 @@ import { environment } from 'src/environments/environment';
         HttpClientModule,
         StoreModule.forRoot(reducers, { metaReducers: [stateSetter] }),
         EffectsModule.forRoot([AppEffects, ProductEffects]),
-        !environment.production ? StoreDevtoolsModule.instrument() : []
+        !environment.production ? StoreDevtoolsModule.instrument() : [],
+        BrowserAnimationsModule,
+        MaterialModule
     ],
     providers: [],
     bootstrap: [AppComponent]
